98ce44faaef4270567a62936bd2f682be3c2acbf
tiagoenriquez <tiagoenriquez@gmail.com>
2020-12-24 12:34:26 -0300

    editando cadastro de reservas

--- src/app/cadastro-reserva/cadastro-reserva.page.ts
+++ src/app/cadastro-reserva/cadastro-reserva.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/models/Usuario';
+import { ReservaService } from 'src/services/ReservaService';
+import { ReservaData } from 'src/viewsModels/ReservaData';
 
 @Component({
   selector: 'app-cadastro-reserva',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroReservaPage implements OnInit {
 
-  constructor() { }
+  constructor(private reservaService: ReservaService) { }
+
+  public usuario: Usuario = new Usuario();
+  public data: string;
+  public reservas: ReservaData[] = new Array<ReservaData>();
+  public horarios: string[] = new Array<string>();
 
   ngOnInit() {
+    this.obterUsuarioLogado();
+    this.obterData();
+    this.listar(this.data);
+    this.obterHorariosDisponiveis();
+  }
+  
+  obterUsuarioLogado() {
+    const usuarioLogado: Usuario = JSON.parse(localStorage.getItem('usuarioLogado'));
+    if(usuarioLogado) this.usuario = usuarioLogado;
+  }
+
+  obterData() {
+    const dataObtida: string = JSON.parse(localStorage.getItem('data'));
+    if(dataObtida) this.data = dataObtida;
+    else this.data = Date();
+  }
+
+  listar(data: string) {
+    console.log(data);
+    let subscribe = this.reservaService.listarPorData(data).subscribe((res) => {
+      this.reservas = res;
+      console.log(this.reservas);
+    });
+  }
+
+  obterHorariosDisponiveis() {
+    console.log(this.reservas);
   }
 
 }
--- src/app/lista-reservas/lista-reservas.page.ts
+++ src/app/lista-reservas/lista-reservas.page.ts
@@ -13,6 +13,7 @@ export class ListaReservasPage implements OnInit {
 
   public reservas: ReservaData[] = new Array<ReservaData>();
   public data: string;
+  public horariosDisponiveis: string[] = new Array<string>();
 
   constructor(private reservaService: ReservaService, private router: Router) { }
 
@@ -22,24 +23,45 @@ export class ListaReservasPage implements OnInit {
   ionViewDidEnter() {
     this.data = Date();
     this.data = DateFormat.convertDateTimePrint(this.data);
-    console.log(this.data);
     this.listar(this.data);
+    this.obterHorariosDisponiveis();
   }
 
   listar(data: string) {
-    console.log(data);
     let subscribe = this.reservaService.listarPorData(data).subscribe((res) => {
       this.reservas = res;
     });
   }
 
-  reservar(data: string) {
-    localStorage.setItem('data', JSON.stringify(data));
+  reservar() {
+    console.log(this.data);
+    localStorage.setItem('data', JSON.stringify(this.data));
     this.router.navigate(['/cadastro-reserva']);
   }
 
   obterHorario(data: string) {
     return DateFormat.convertTimePrint(data);
+  }
+
+  obterHorariosDisponiveis() {
+    let subscribe = this.reservaService.listarPorData(this.data).subscribe((res) => {
+      this.reservas = res;
+      let data = Date();
+      let dataAtual = Number(data[8] + data[9]);
+      let horaAtual = Number(data[16] + data[17]);
+      let horariosPossiveis = [6, 8, 10, 12, 14, 16, 18, 20];
+      for(let reserva of this.reservas) {
+        let data = Number(reserva.inicio[8] + reserva.inicio[9]);
+        let hora = Number(reserva.inicio[11] + reserva.inicio[12]);
+        for(let i = 0; i < horariosPossiveis.length; i++) {
+          console.log(horariosPossiveis[i]);
+          if(horariosPossiveis[i] != hora || (horariosPossiveis[i] < horaAtual && data == dataAtual) || data < dataAtual) {
+            horariosPossiveis.splice(horariosPossiveis[i]);
+          }
+        }
+        console.log(horariosPossiveis);
+      }
+    });
   }
 
 }
